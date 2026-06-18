bb5edaeaf105895914b2a1d00b79f01ef8d54bd7
Karl <karlcaparal05@gmail.com>
2026-06-18 11:29:35 +0800

    done

---
+++ app/(tabs)/subscriptions.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const subscriptions = () => {
+  return (
+    <View>
+      <Text>subscription</Text>
+    </View>
+  )
+}
+
+export default subscriptions
