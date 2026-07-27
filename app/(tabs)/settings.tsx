4c44bdb50c3dca4f9ad47eab319fa1c4e2819c7b
Karl <karlcaparal05@gmail.com>
2026-07-27 09:22:41 +0800

    Features: clerk auth implementation

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,28 +1,22 @@
-/*import { styled } from "nativewind";
-import React from 'react';
-import { Text } from 'react-native';
-import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
-const SafeAreaView = styled(RNSafeAreaView);
+import { useClerk } from '@clerk/expo'
+import { styled } from 'nativewind'
+import React from 'react'
+import { Pressable, Text } from 'react-native'
+import { SafeAreaView as RNSafeAreaView } from 'react-native-safe-area-context'
 
-const settings = () => {
+const SafeAreaView = styled(RNSafeAreaView)
+
+const Settings = () => {
+  const { signOut } = useClerk()
+
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text>settings</Text>
+      <Text className="list-title mb-6">Settings</Text>
+      <Pressable className="auth-button" onPress={() => signOut()}>
+        <Text className="auth-button-text">Sign Out</Text>
+      </Pressable>
     </SafeAreaView>
   )
 }
 
-export default settings*/
-
-import React from 'react'
-import { Text, View } from 'react-native'
-
-const settings = () => {
-  return (
-    <View>
-      <Text>settings</Text>
-    </View>
-  )
-}
-
-export default settings
+export default Settings
